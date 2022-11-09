c3d36315b64a72bc78819409c5ede0e9874c060f
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-11-09 20:44:53 +0530

    working on signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,22 @@ import {
   selectUserPhoto,
   setUserLoginDetails,
 } from "../features/user/userSlice";
+import { useEffect } from "react";
 const Header = (props) => {
   const dispatch = useDispatch();
   const history = useNavigate();
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history("/home");
+      }
+    });
+  }, [userName]);
+
   const handleAuth = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
@@ -70,7 +81,12 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <UserImg src={userPhoto} alt={userName} />
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign Out</span>
+            </DropDown>
+          </SignOut>
         </>
       )}
     </Nav>
@@ -186,6 +202,27 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
 `;
 
 export default Header;
